81e8f83ac8d44b6ef724348161cfdb8594de4e3a
Blake Loring <blake_l@parsed.uk>
2015-06-07 19:24:59 +0100

    Fixed up lamdas to filter and minimize in a single line

--- src/conn.rs
+++ src/conn.rs
@@ -1,4 +1,27 @@
+use std::cmp;
+use std::cmp::Ordering;
+
 pub struct Conn {
 	pub dest: i32,
 	pub cost: i32
+}
+
+impl cmp::PartialOrd for Conn {
+	fn partial_cmp(&self, other: &Conn) -> Option<Ordering> {
+		return Some(self.dest.cmp(&other.dest));
+	}
+}
+impl cmp::PartialEq for Conn {
+	fn eq(&self, other: &Conn) -> bool {
+		return self.dest == other.dest;
+	}
+}
+
+impl cmp::Eq for Conn {
+}
+
+impl cmp::Ord for Conn {
+	fn cmp(&self, other: &Self) -> Ordering {
+		return self.dest.cmp(&other.dest);
+	}
 }
--- src/graph_walker.rs
+++ src/graph_walker.rs
@@ -2,10 +2,6 @@ use std::collections::HashMap;
 use std::vec::Vec;
 use conn::Conn;
 
-fn first_usable<'a>(current : &'a Vec<Conn>, result : &Vec<i32>) -> Option<&'a Conn> {
-	return current.iter().find(|&x| result.iter().find(|&y| *y == x.dest).is_none());
-}
-
 pub fn find_path(start : i32, end : i32, map : &HashMap<i32, Vec<Conn>>) -> Vec<i32> {
 	let mut current = start;
 	let mut result = Vec::new();
@@ -13,17 +9,8 @@ pub fn find_path(start : i32, end : i32, map : &HashMap<i32, Vec<Conn>>) -> Vec<
 	result.push(start);
 
 	while current != end {
-		
 		let ref paths = map[&current];
-		let mut shortest = first_usable(paths, &result).unwrap();
-
-		for path in paths {
-			let can_use = result.iter().find(|&x| *x == path.dest).is_none();
-			if can_use && (path.cost < shortest.cost) {
-				shortest = path;
-			}
-		}
-
+		let shortest = paths.iter().filter(|&x| result.iter().find(|&y| *y == x.dest).is_none()).min().unwrap();
 		current = shortest.dest;
 		result.push(current);
 	}
