c42202af74a0c342e74c10cc35ecbba09f4fb307
Blake Loring <blake_l@parsed.uk>
2015-06-07 14:29:51 +0100

    Generating random values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate rand;
+
+use std::collections::HashMap;
+use rand::Rng;
+
+mod other;
+
+struct Conn {
+	dest: i32,
+	cost: i32
+}
+
 fn main() {
 	println!("Hello World!\n");
+	other::poo();
+
+	let mut rng = rand::thread_rng();
+
+	let mut map = HashMap::new();
+
+	for x in 0..100 {
+		for y in 0..100 {
+			map.insert(x, Conn{dest: y, cost: rng.gen::<i32>() % 150});
+		}
+	}
+
+	for link in map {
+		let (_, x) = link;
+		println!("{} {}", x.dest, x.cost);
+	}
 }
---
+++ src/other.rs
@@ -0,0 +1,4 @@
+
+pub fn poo() {
+	println!("EWwwwwwWWWW you stinky shit");	
+}
