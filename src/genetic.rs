be00d7b87ee0d9909f78bdfb5ec4573128cf5b63
Blake Loring <blake_l@parsed.uk>
2015-06-08 09:35:05 +0100

    Calculate costs

--- src/genetic.rs
+++ src/genetic.rs
@@ -4,6 +4,7 @@ use conn::Conn;
 use rand::thread_rng;
 use rand::distributions::{IndependentSample, Range};
 use graph_walker;
+use graph_printer;
 
 /**
  * Generate a walk using a genetic heuristic
@@ -11,4 +12,5 @@ use graph_walker;
 pub fn genetic(start : i32, end : i32, map : &HashMap<i32, Vec<Conn>>) -> Vec<i32> {
   const population_size : usize = 500;
   let initial [Vec<i32>, population_size] = [graph_walker::random_walk(start, end, map); population_size];
+  let costs = initial.iter().map(|&x| graph_printer::total_cost(start, x, map));
 }
