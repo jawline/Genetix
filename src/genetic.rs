b3154ea12ad4999db079d9d3f154140a830408e2
Blake Loring <blake_l@parsed.uk>
2015-06-08 09:27:27 +0100

    Update genetic.rs

--- src/genetic.rs
+++ src/genetic.rs
@@ -9,4 +9,6 @@ use graph_walker;
  * Generate a walk using a genetic heuristic
  */
 pub fn genetic(start : i32, end : i32, map : &HashMap<i32, Vec<Conn>>) -> Vec<i32> {
+  const population_size : usize = 500;
+  let initial [Vec<i32>, population_size] = [graph_walker::random_walk(start, end, map); population_size];
 }
