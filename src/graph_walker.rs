0e8564a2c28cb223caa80a69bdf4f140fa6c9d2c
Blake Loring <blake_l@parsed.uk>
2015-06-09 09:48:19 +0100

    Update graph_walker.rs

--- src/graph_walker.rs
+++ src/graph_walker.rs
@@ -48,6 +48,7 @@ pub fn niave_cut_cycle(path : &mut Vec<i32>) {
 		}
 
 		//Recalculate the count map and look for the next iter (We may have inadvertantly removed doubles already)
+		count.clear();
 		for item in path {
 			count[item] += 1;
 		}
