3e3a68be0c07d4d518bd83754bf2f9266d6df2e2
Blake Loring <blake_l@parsed.uk>
2015-06-10 20:33:41 +0100

    Shaking up the weightings

--- src/graph_maker.rs
+++ src/graph_maker.rs
@@ -11,7 +11,13 @@ pub fn make_graph() -> HashMap<i32, Vec<conn::Conn>> {
 			if y == x {
 				links.push(conn::Conn{dest: y, cost: 0});
 			} else {
-				links.push(conn::Conn{dest: y, cost: y});
+				if x == 0 && y == 30 {
+					links.push(conn::Conn{dest: y, cost: 999999});
+				} else if x == 0 || x % 2 != 0 {
+					links.push(conn::Conn{dest: y, cost: y});
+				} else {
+					links.push(conn::Conn{dest: y, cost: 999999});
+				}
 			}
 		}
 		map.insert(x, links);
