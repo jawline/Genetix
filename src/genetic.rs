513460195c77aab258488f1fe22f09f9912328fb
Blake Loring <blake_l@parsed.uk>
2015-06-10 10:24:24 +0100

    Update genetic.rs

--- src/genetic.rs
+++ src/genetic.rs
@@ -1,23 +1,14 @@
 use conn::Conn;
 use std::collections::HashMap;
 use std::vec::Vec;
+use graph_printer;
 use graph_walker;
 
 /**
  * Generate a walk using dijkstras
  */
 pub fn genetic(start : i32, end : i32, map : &HashMap<i32, Vec<Conn>>) -> Vec<i32> {
-	let mut current = start;
-	let mut result = Vec::new();
-
-	result.push(start);
-
-	while current != end {
-		let ref paths = map[&current];
-		let shortest = paths.iter().filter(|&x| result.iter().find(|&y| *y == x.dest).is_none()).min().unwrap();
-		current = shortest.dest;
-		result.push(current);
-	}
-
-	return result;
+    let mut populations = [graph_walker::random_walk(start, end, map); 500];
+    populations.sort_by(|&x &y| graph_printer::total_cost(x) < graph_printer::total_cost(y));
+    return populations[0];
 }
