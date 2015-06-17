a62c092d2ba62adbfbb54ea7d95776dc6de3f7b1
Blake Loring <blake_l@parsed.uk>
2015-06-17 14:19:59 +0100

    Update graph_maker.rs

--- src/graph_maker.rs
+++ src/graph_maker.rs
@@ -1,9 +1,13 @@
+extern crate rand;
+
 use std::collections::HashMap;
 use std::vec::Vec;
+use rand::thread_rng;
 use conn;
 
 pub fn make_graph() -> HashMap<i32, Vec<conn::Conn>> {
 	let mut map = HashMap::new();
+	let mut rng = rand::thread_rng();
 
 	for x in 0..1000 {
 		let mut links = Vec::new();
@@ -11,13 +15,10 @@ pub fn make_graph() -> HashMap<i32, Vec<conn::Conn>> {
 			if y == x {
 				links.push(conn::Conn{dest: y, cost: 0});
 			} else {
-				if x == 0 && y == 30 {
-					links.push(conn::Conn{dest: y, cost: 999999});
-				} else if x == 0 || x % 2 != 0 {
-					links.push(conn::Conn{dest: y, cost: y});
-				} else {
-					links.push(conn::Conn{dest: y, cost: 999999});
-				}
+				links.push(conn::Conn{
+					dest: y,
+					cost: Range::new(0, 8000).ind_sample(&mut rng)
+				});
 			}
 		}
 		map.insert(x, links);
