d8dce4beca251301fa759af6852b31b7a526c64a
Blake Loring <blake_l@parsed.uk>
2015-06-18 10:41:35 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ mod genetic;
 
 fn main() {
 	let map = graph_maker::make_graph();
+	
+	let random_path = graph_walker::random_walk(0, 500, &map);
+	graph_printer::output_path(&path, &map);
 
 	let path = graph_walker::dijkstras(0, 30, &map);
 	graph_printer::output_path(&path, &map);
@@ -15,5 +18,5 @@ fn main() {
 	let genetic_path = genetic::genetic(0, 30, &map);
 	graph_printer::output_path(&genetic_path, &map);
 	
-	println!("Optimal: {} Genetic: {}", graph_printer::total_cost(&path, &map), graph_printer::total_cost(&genetic_path, &map));
+	println!("Optimal: {} Genetic: {} Random: {}", graph_printer::total_cost(&path, &map), graph_printer::total_cost(&genetic_path, &map), graph_printer::total_cost(&random_path, &map));
 }
