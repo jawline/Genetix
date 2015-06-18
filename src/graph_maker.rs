e8d1c703d8082b2c37a54ff58bab72f67f8ca8e3
Blake Loring <blake_l@parsed.uk>
2015-06-18 13:40:17 +0100

    Update graph_maker.rs

--- src/graph_maker.rs
+++ src/graph_maker.rs
@@ -12,14 +12,13 @@ pub fn make_graph() -> HashMap<i32, Vec<conn::Conn>> {
 	
 	const MAX_LINK_COST : i32 = 8000;
 	const MAX_ITEMS : i32 = 1000;
-	const MAX_LINKS_PER_ITEM : i32 = 400;
 	
 	let number_of_items = Range::new(1, MAX_ITEMS).ind_sample(&mut rng);
 
 	for x in 0..number_of_items {
 		let number_of_links = Range::new(1, MAX_LINKS_PER_ITEM).ind_sample(&mut rng);
 		let mut links = Vec::new();
-		for y in 0..number_of_links {
+		for y in 0..items {
 			if y == x {
 				links.push(conn::Conn{dest: y, cost: 0});
 			} else {
